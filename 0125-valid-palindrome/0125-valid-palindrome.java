950fbf17658db24851cd6720daaac1d5d67b2716
Shreeshail Miragi <shreeshailbm18@gmail.com>
2026-06-27 00:26:41 +0530

    Time: 3 ms (60.61%), Space: 44.3 MB (75.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
+        int right = s.length() - 1;
         s = s.toLowerCase();
-        s = s.replaceAll("[^a-z0-9]","");
-        int right = s.length();
-        for(int i=0;i < right/2;i++){
-            if(s.charAt(i) != s.charAt(right-1-i)){
-                return false;
+
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++; // Move forward if not alphanumeric
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--; // Move backward if not alphanumeric (Fixed bug #1)
+            } else {
+                // If they don't match, it is definitively NOT a palindrome
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false; 
+                }
+                // If they match, move both pointers inward to check next chars
+                left++;
+                right--;
             }
         }
-        return true;
+        // If the loop finishes without returning false, it IS a palindrome
+        return true; 
     }
 }

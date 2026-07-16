7a58f7e525bf2f9e439b1b4e3351acbfdc10c45b
Shreeshail Miragi <shreeshailbm18@gmail.com>
2026-07-17 01:12:33 +0530

    Time: 22 ms (6.96%), Space: 46.9 MB (5.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }else{
+                map.put(s.charAt(i),1);
+            }
+            
+        }
+
+        for(int i=0;i<t.length();i++){
+            if(!map.containsKey(t.charAt(i))){
+                return false;
+            }
+
+            map.put(t.charAt(i), map.get(t.charAt(i))-1);
+
+            if(map.get(t.charAt(i)) < 0)
+                return false;
+        }
+        return true;
+    }
+}

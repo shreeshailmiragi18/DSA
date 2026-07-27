19b95a0eb3a1323ee5fb0c14e62015529e5a8ae5
Shreeshail Miragi <shreeshailbm18@gmail.com>
2026-07-28 02:09:16 +0530

    Time: 0 ms (100%), Space: 44 MB (44.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        boolean result = false;
+    boolean result = false;
         for(int i=0;i<matrix.length;i++){
-            if(target <= matrix[i][matrix[0].length-1]){
+            if(target <= matrix[i][matrix[0].length-1] && target >= matrix[i][0]){
                 for(int j=0;j<matrix[0].length;j++){
                     if(matrix[i][j] == target){
                         result = true;
@@ -11,6 +11,23 @@ class Solution {
             }
         }
         return result;
+
+
+
+
+
+
+        // boolean result = false;
+        // for(int i=0;i<matrix.length;i++){
+        //     if(target <= matrix[i][matrix[0].length-1]){
+        //         for(int j=0;j<matrix[0].length;j++){
+        //             if(matrix[i][j] == target){
+        //                 result = true;
+        //             }
+        //         }
+        //     }
+        // }
+        // return result;
 
 
 

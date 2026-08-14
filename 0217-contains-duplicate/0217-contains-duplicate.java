0d977ceee79d90cce8958a304bc4c2771462516d
Shreeshail Miragi <shreeshailbm18@gmail.com>
2026-08-14 23:58:59 +0530

    Time: 18 ms (69.47%), Space: 108.2 MB (38.48%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,33 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        TreeSet<Integer> set = new TreeSet<>();
-        set.add(nums[0]);
-        for(int i=1;i<nums.length;i++){
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
             if(set.contains(nums[i])){
                 return true;
-            }
+            }else
             set.add(nums[i]);
         }
         return false;
+
+
+        // for(int i=0;i<nums.length-1;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // TreeSet<Integer> set = new TreeSet<>();
+        // set.add(nums[0]);
+        // for(int i=1;i<nums.length;i++){
+        //     if(set.contains(nums[i])){
+        //         return true;
+        //     }
+        //     set.add(nums[i]);
+        // }
+        // return false;
     }
 }

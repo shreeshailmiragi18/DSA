5ccfa70178a561209afdc67bb9b8c96b261c820e
Shreeshail Miragi <shreeshailbm18@gmail.com>
2026-09-09 22:21:17 +0530

    Time: 1 ms (83.58%), Space: 42.5 MB (56.6%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,23 +3,14 @@ class Solution {
 
         int count = 0;
 
-        int start = 1000;  // First number that needs a comma
+        int start = 1000;
         int commas = 1;
 
         while (start <= n) {
-
             int end = start * 1000 - 1;
-
-            // Don't go beyond n
             int limit = Math.min(n, end);
-
-            // Number of numbers in this range
             int numbers = limit - start + 1;
-
-            // Each number has 'commas' commas
             count += numbers * commas;
-
-            // Move to the next comma level
             start *= 1000;
             commas++;
         }
